9ba24fe59e0ec9306168acb904c50649a3dcaac5
Peter Szabo <pts@fazekas.hu>
2018-04-29 00:07:16 +0200

    added full implementation of --backend=gpg; made --backend=gpg the default; added invocation example to README.txt

--- README.txt
+++ README.txt
@@ -5,9 +5,24 @@ passphrases, creating a self-decrypting Bash or Zsh shell script, which asks
 for the passphrase, and if it matches any of the passphrases used for
 encryption, then decrypts and executes the original script.
 
-encrypt_script.bash and the encrypted scripts it creates use the openssl(1)
-command-line tool with the aes-256-cbc crypto. They also use Bash
-(/bin/bash, also works with Zsh) and /dev/tty . There is no other dependency.
+Example usage for creating an encrypted script:
+
+  $ echo 'echo hi"!$@"; id "$@"' >plain.sh
+  $ :>passphrase.in; chmod 600 passphrase.in
+  $ echo secret >passphrase.in
+  $ ./encrypt_script.bash --out=encrypted.sh passphrase.in <plain.sh
+  $ ./encrypted.sh
+  enter bash-script passphrase:
+  fatal: incorrect bash-script passphrase
+  $ ./encrypted.sh
+  enter bash-script passphrase:
+  hi!
+  ...
+
+encrypt_script.bash and the encrypted scripts it creates use the gpg(1) (or
+openssl(1) with -aes-256-cbc if --backend=openssl is specified) command-line
+tool. They also use Bash (/bin/bash, also works with Zsh) and /dev/tty .
+There is no other dependency.
 
 encrypt_script.bash and the encrypted scripts don't create any temporary
 files.
@@ -15,7 +30,7 @@ files.
 encrypt_script.bash and the encrypted scripts don't put passphrases into the
 argv or to the environment (so users running ps(1) can't inspect it).
 
-encrypt_script.bash uses openssl(1) with a random salt by default, thus the
+encrypt_script.bash uses gpg(1) (or openssl(1)) with a random salt, thus the
 output file will be different even after rerunning for the same input.
 
 The encrypted scripts need Bash (rather than e.g. Busybox sh or Dash) to
@@ -30,17 +45,24 @@ How secure are scripts encrypted by encrypt_script.sh?
 * good: The passphrase doesn't show up in argv or environment variables, so
   other processes running in the same system can't easily inspect it with
   /proc (but root can create a memory dump to inspect it).
-* bad: The key derivation scheme is weak, a dictionary attack is much faster
-  than on `gpg --symmetric'.
-* bad: There is no integrity protection, so the middle of the encrypted
-  script can be modified without knowing the passphrase, and this won't be
-  detected at extraction time. (By modifying 1 bytes, 20 or more bytes in
-  the decrypted stream may get ruined.) `gpg --symmetric --force-mdc` does
+* good: Crypto is as secure as the default gpg settings where
+  encrypt_script.bash is run (usually good).
+* bad for --backend=openssl: The key derivation scheme is weak, a dictionary
+  attack is much faster than with the default --backend=gpg .
+* bad for --backend=openssl: There is no integrity protection, so the middle
+  of the encrypted script can be modified without knowing the passphrase,
+  and this won't be detected at extraction time. (By modifying 1 bytes, 20
+  or more bytes in the decrypted stream may get ruined.) --backend=gpg does
   provide integrity protection.
 
 Encrypted scripts are not binary-safe. They are safe if the script to be
 encrypted doesn't contain the 0 byte (ASCII NUL). Bash ignores this byte in
 some context such as here-documents (<<'END'). Zsh doesn't ignore it.
+
+gpg version compatibility:
+
+* gpg-1.4.10 (2008): OK
+* gpg-2.2.2 (2017): OK
 
 openssl version compatibility:
 
--- encrypt_script.bash
+++ encrypt_script.bash
@@ -26,7 +26,7 @@ Flags:
 fi
 
 OUT=
-BACKEND=openssl
+BACKEND=gpg
 PTYPE=bash-script
 while test $# != 0; do
   if test "${1#--out=}" != "$1"; then
@@ -54,13 +54,12 @@ test "$OUT" || die 'missing --out=...'
 test $# = 0 && die 'missing <passphrase-file>'
 
 if test "$BACKEND" = gpg; then
+  # This may display a migrating... ~/.gnupg message. It's hard ot disable.
   D=$(gpg -d -q --batch --passphrase a <<<'-----BEGIN PGP MESSAGE-----
 
 jA0EAwMC+Gv+j4hMGX5g0joB7u8WLHTg0eLf3Rl1IvUkIXvsYGIDLvdN3M6m0sBgvXLFHby5D+CjaTtfW7t8OdQT+ljyJgQSVjyB=6nE9
 -----END PGP MESSAGE-----')
   test "$D" = unencumbered || die 'gpg -d -q --batch --passphrase a is broken'
-  die '--backend=gpg not supported yet'
-  exit 42
 else
   D="$(command openssl enc -a -d -aes-256-cbc -k a -nosalt -md sha1 <<<'M1/LvAYWRMW2kWce+uoEBQ==')"
   test "$D" = unencumbered || die 'openssl enc -a -d -aes-256-cbc -md sha1 is broken'
@@ -80,17 +79,35 @@ echo "#!/bin/bash --
 function die() {
   echo \"fatal: \$*\" >&2
   exit 1
-}
-if ! type -p openssl 2>/dev/null >&2; then
+}"
+# TODO(pts): Disable installation.
+if test "$BACKEND" = gpg; then
+echo "if ! type -p gpg 2>/dev/null >&2; then
+  echo 'info: gpg not found for $PTYPE, installing' >&2
+  command sudo apt-get install gnupg
+  type -p gpg 2>/dev/null >&2 || die 'gpg: command not found'
+fi"
+else
+echo "if ! type -p openssl 2>/dev/null >&2; then
   echo 'info: openssl not found for $PTYPE, installing' >&2
   command sudo apt-get install openssl
   type -p openssl 2>/dev/null >&2 || die 'openssl: command not found'
 fi"
+fi
 # unset is used to unexport, i.e. remove from the environment.
-echo "unset PP EP D D1 C
-D=\"\$(command openssl enc -a -d -aes-256-cbc -k a -nosalt -md sha1 <<<'M1/LvAYWRMW2kWce+uoEBQ==' 2>/dev/null)\"
-test \"\$D\" = unencumbered || die 'openssl enc -a -d -aes-256-cbc -md sha1 is broken'
-echo -n \"enter $PTYPE passphrase: \" >&2
+echo "unset PP EP D D1 C"
+if test "$BACKEND" = gpg; then
+  # This may display a migrating... ~/.gnupg message. It's hard ot disable.
+  echo "D=\$(gpg -d -q --batch --passphrase a <<<'-----BEGIN PGP MESSAGE-----
+
+jA0EAwMC+Gv+j4hMGX5g0joB7u8WLHTg0eLf3Rl1IvUkIXvsYGIDLvdN3M6m0sBgvXLFHby5D+CjaTtfW7t8OdQT+ljyJgQSVjyB=6nE9
+-----END PGP MESSAGE-----')
+test \"\$D\" = unencumbered || die 'gpg -d -q --batch --passphrase a is broken'"
+else
+  echo "D=\"\$(command openssl enc -a -d -aes-256-cbc -k a -nosalt -md sha1 <<<'M1/LvAYWRMW2kWce+uoEBQ==' 2>/dev/null)\"
+test \"\$D\" = unencumbered || die 'openssl enc -a -d -aes-256-cbc -md sha1 is broken'"
+fi
+echo "echo -n \"enter $PTYPE passphrase: \" >&2
 read -s PP </dev/tty
 echo >&2
 test \"\$PP\" || exit 1
@@ -100,22 +117,73 @@ for PF in "$@"; do
 PP=
 read PP <"$PF"
 test "$PP" || die "empty or missing passphrase in file: $PF"
-PE="$(command openssl enc -a -aes-256-cbc -kfile <(echo -n "$PP") -md sha1 <<<"$D $D")"
-test "$?" = 0 || die 'openssl enc on passphrase failed'
-test "$PE" || die 'openssl enc on passphrase returned empty output'
-echo "'${PE//
+if test "$BACKEND" = gpg; then
+  PE="$(command gpg -c -q -a --batch --force-mdc -z 9 --passphrase-file <(echo -n "$PP") <<<"$D.")"
+  test "$?" = 0 || die 'gpg -c on passphrase failed'
+  test "$PE" || die 'gpg -c on passphrase returned empty output'
+  test "${PE#-----BEGIN PGP MESSAGE-----
+}" = "$PE" && die 'gpg -c -a on passphrase returned output without ASCII armor at the beginning'
+  test "${PE#*
+
+}" = "$PE" && die 'gpg -c -a on passphrase returned output without header at the beginning'
+  test "${PE%
+-----END PGP MESSAGE-----}" = "$PE" && die 'gpg -c -a on passphrase returned output without ASCII armor at the end'
+  PE="${PE#*
+
+}"
+  PE="${PE%
+-----END PGP MESSAGE-----}"
+  echo "'${PE//
+/}' \\"
+else
+  PE="$(command openssl enc -a -aes-256-cbc -md sha1 -kfile <(echo -n "$PP") <<<"$D $D")"
+  test "$?" = 0 || die 'openssl enc on passphrase failed'
+  test "$PE" || die 'openssl enc on passphrase returned empty output'
+  echo "'${PE//
 / }' \\"
+fi
 done
-echo "; do
-  read D < <(command openssl enc -a -d -aes-256-cbc -kfile <(echo -n \"\$PP\") -md sha1 <<<\"\${EP// /
-}\" 2>/dev/null)
-  test \"\$?\" = 0 && D1=\"\${D%% *}\" && test \"\$D1\" = \"\${D#* }\" && test \"\$D\" = \"\$D1 \$D1\" && D=\"\$D1\" && break
-  D=
+if test "$BACKEND" = gpg; then
+  echo "; do
+  read D < <(gpg -d -q --batch --passphrase-file <(echo -n \"\$PP\") 2>/dev/null <<<\"-----BEGIN PGP MESSAGE-----
+
+\$EP
+-----END PGP MESSAGE-----\")
+  test \"\$?\" = 0 && test \"\${D%.}\" != \"\$D\" && D=\"\${D%.}\" && break"
+  DECRYPT_CMD="gpg -d -q --batch --passphrase-file"
+else
+  echo "; do
+  read D < <(command openssl enc -a -d -aes-256-cbc -md sha1 -kfile <(echo -n \"\$PP\") 2>/dev/null <<<\"\${EP// /
+}\")
+  test \"\$?\" = 0 && D1=\"\${D%% *}\" && test \"\$D1\" = \"\${D#* }\" && test \"\$D\" = \"\$D1 \$D1\" && D=\"\$D1\" && break"
+  DECRYPT_CMD="openssl enc -a -d -aes-256-cbc -md sha1 -kfile"
+fi
+echo "  D=
 done
 test \"\$D\" || die 'incorrect $PTYPE passphrase'
-C=\"unset C;\$(command openssl enc -a -d -aes-256-cbc -kfile <(echo -n \"\$D\") -md sha1 2>/dev/null <<'HEREND'"
-(read LINE; test "${LINE#\#!}" = "$LINE" || LINE=; echo "$LINE"; command cat) |
-    command openssl enc -a -aes-256-cbc -kfile <(echo -n "$D") -md sha1 || die "openssl enc on payload failed"
+C=\"unset C;\$(command $DECRYPT_CMD <(echo -n \"\$D\") <<'HEREND'"
+function script_source() {
+  read LINE; test "${LINE#\#!}" = "$LINE" || LINE=; echo "$LINE"; command cat
+}
+if test "$BACKEND" = gpg; then
+# Also `-z 0' can be used to disable compression.
+DE="$(script_source | command gpg -c -q -a --batch --force-mdc --s2k-mode 1 --passphrase-file <(echo -n "$D"))"
+test "$?" = 0 || die "gpg -c on script failed"
+test "${DE#-----BEGIN PGP MESSAGE-----
+}" = "$DE" && die 'gpg -c -a on script returned output without ASCII armor at the beginning'
+test "${DE#*
+
+}" = "$DE" && die 'gpg -c -a on script returned output without header at the beginning'
+test "${DE%
+-----END PGP MESSAGE-----}" = "$DE" && die 'gpg -c -a on script returned output without ASCII armor at the end'
+  echo "-----BEGIN PGP MESSAGE-----
+
+${DE#*
+
+}"
+else
+script_source | command openssl enc -a -aes-256-cbc -md sha1 -kfile <(echo -n "$D") || die "openssl enc on script failed"
+fi
 echo "HEREND
 )\"
 test \"\$?\" = 0 || die 'decrypt failed'
